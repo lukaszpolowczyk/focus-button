1c1851c0a61bddbc2ba709a7f624b818f5233f13
Łukasz Polowczyk <dev.lukaszpolowczyk@gmail.com>
2021-04-11 17:27:19 +0200

    first commit

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { onMount } from 'svelte';
+	import '../app.css';
+	
+	let b;
+	onMount(()=>b.focus());
+</script>
+<button bind:this={b}>focused</button>
+<slot />
+
---
+++ src/routes/[slug].svelte
@@ -0,0 +1,13 @@
+<!-- [slug].svelte -->
+<svelte:head><title>Lost focus - Sveltekit Routers</title></svelte:head>
+<script>
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+		
+  import Keydown from "svelte-keydown";
+</script>
+<Keydown on:ArrowRight={() => goto(`${Number($page.params.slug)+1}`) } />
+<br>
+<button on:click={()=>goto(`${Number($page.params.slug)+1}`)}>goto({Number($page.params.slug)+1})</button>
+or press RightArrow
+
